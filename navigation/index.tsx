19eeb7b8e866b25d95e53a6df253ff8cf3f538be
Usuario <javichua.a@hotmail.com>
2022-10-14 11:33:41 +0200

    primer commit

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -66,8 +66,8 @@ function BottomTabNavigator() {
         name="TabOne"
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Login',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
@@ -89,7 +89,7 @@ function BottomTabNavigator() {
         component={TabTwoScreen}
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="github" color={color} />,
         }}
       />
     </BottomTab.Navigator>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,81 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import React from 'react';
+import { Button, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.containerbig}
+    >
+      <View style={styles.container}>
+        <View style={styles.panel}>
+          <View style={styles.divs}>
+            <Text style={styles.title}>Name</Text>
+            <TextInput style={styles.input} placeholder="Enter your Name"></TextInput>
+          </View>
+          <View style={styles.divs}>
+            <Text style={styles.title}>Password</Text>
+            <TextInput style={styles.input} placeholder="Enter your Password" secureTextEntry={true}></TextInput>
+          </View>
+          <View>
+             <Button title='Login' color={"blue"}></Button>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footer_text}>Prueba realizada para crear una pequeña interfaz de logeo usando los recursos de React-Native.Prueba realizada para crear una pequeña interfaz de logeo usando los recursos de React-Native.</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  containerbig:{
+    flex:1
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#99ffff',
+  },
+  footer:{
+    backgroundColor: '#99ffff',
+    padding: 5
+  },
+  footer_text:{
+    textAlign:"center",
+    fontSize:10
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  input:{
+    backgroundColor:"#ffffff",
+    width:"99%",
+    textAlign: "center",
+    height:"35%",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    fontSize:18,
+  },
+  panel:{
+    width: "89%",
+    height: "45%",
+    borderRadius:20,
+    borderWidth:1,
+    borderColor:"black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  divs:{
+    width: "85%",
+    height: "40%",
   },
 });
